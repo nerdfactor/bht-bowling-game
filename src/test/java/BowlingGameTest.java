10587e36c7de56ef3406ff9a4e5539ae48ee1886
Daniel Klug <mail@nerdfactor.eu>
2023-12-02 09:32:52 +0100

    added test to check if basic scoring works.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -18,4 +18,20 @@ class BowlingGameTest {
 		int totalScore = game.countScore();
 		Assertions.assertEquals(expectedScore, totalScore);
 	}
+
+	/**
+	 * Check if the total score of the game is counted correctly using
+	 * the same amount of knocked over pins for each roll.
+	 */
+	@Test
+	void countTotalScoreOfKnockedOverPins() {
+		int expectedScore = 60;
+		int amountOfRolls = 20;
+		BowlingGame game = new BowlingGame();
+		for (int currentRoll = 0; currentRoll < amountOfRolls; currentRoll++) {
+			game.nextRoll(3);
+		}
+		int totalScore = game.countScore();
+		Assertions.assertEquals(expectedScore, totalScore);
+	}
 }
