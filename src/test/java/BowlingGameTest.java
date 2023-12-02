38f3aa2a67c8060a4d8df02218dc9a998bd5df0c
Daniel Klug <mail@nerdfactor.eu>
2023-12-02 10:30:33 +0100

    added Test with random amount of knocked over pins.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -2,6 +2,8 @@ import eu.nerdfactor.BowlingGame;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 class BowlingGameTest {
 
 	/**
@@ -48,6 +50,39 @@ class BowlingGameTest {
 		}
 		int totalScore = game.countScore();
 		Assertions.assertEquals(expectedScore, totalScore);
+	}
+
+	/**
+	 * Check if the total score of the game is counted correctly using
+	 * a random amount of knocked over pins without strikes.
+	 */
+	@Test
+	void countTotalScoreOfRandomKnockedOverPins() {
+		int expectedScore = 0;
+		BowlingGame game = new BowlingGame();
+		for (int currentRoll = 1; currentRoll < BowlingGame.AMOUNT_OF_MAX_ROLLS; currentRoll += 2) {
+			expectedScore += this.rollRandomTestFrame(game);
+		}
+		int totalScore = game.countScore();
+		Assertions.assertEquals(expectedScore, totalScore);
+	}
+
+	/**
+	 * Rolls a complete frame with a randomly selected amount of knocked over pins
+	 * split over two rolls. Makes sure that the frame won't contain a spare or
+	 * a strike.
+	 *
+	 * @param game The game that the rolls will be for.
+	 * @return The amount of randomly selected knocked over pins.
+	 */
+	private int rollRandomTestFrame(BowlingGame game) {
+		int possibleKnockedOverPinsWithoutStrikes = BowlingGame.AMOUNT_OF_PINS - 1;
+		int randomKnockedOverPins = new Random().nextInt(0, possibleKnockedOverPinsWithoutStrikes);
+		int pinsInFirstRoll = randomKnockedOverPins / 2;
+		int pinsInSecondRoll = randomKnockedOverPins - pinsInFirstRoll;
+		game.nextRoll(pinsInFirstRoll);
+		game.nextRoll(pinsInSecondRoll);
+		return randomKnockedOverPins;
 	}
 
 }
