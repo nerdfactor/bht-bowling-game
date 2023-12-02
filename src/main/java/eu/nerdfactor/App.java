4c1ac36a0e26476e56bb194d89db861c95f4b91f
Daniel Klug <mail@nerdfactor.eu>
2023-12-02 11:06:53 +0100

    added a very simple main method to count score for a bowling game.

--- src/main/java/eu/nerdfactor/App.java
+++ src/main/java/eu/nerdfactor/App.java
@@ -1,7 +1,23 @@
 package eu.nerdfactor;
 
+import eu.nerdfactor.exceptions.MaxAmountOfRollsExceededException;
+import eu.nerdfactor.exceptions.WrongAmountOfPinsException;
+
+import java.util.Arrays;
+
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws NumberFormatException, WrongAmountOfPinsException, MaxAmountOfRollsExceededException {
+		System.out.println("Bowling Game");
+		if (args.length > 0) {
+			int[] rolls = Arrays.stream(args[0].split(","))
+					.mapToInt(Integer::parseInt)
+					.toArray();
+			BowlingGame game = new TenPinBowlingGame();
+			game.nextRolls(rolls);
+			int totalScore = game.countScore();
+			System.out.println("Total score of the game is: " + totalScore);
+		} else {
+			System.out.println("Please provide all rolls of the game in a comma separated string.");
+		}
 	}
 }
